b0c1b5facd0e3550fa57e2708b5c61543a567b55
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-11-30 00:10:15 +0100

    added auto-preview

---
+++ README.md
@@ -0,0 +1,15 @@
+# MARKDOWN PREVIEWER
+    This is a cli tool to preview markdown files in the browser before use
+
+## Usage
+    you pass in the markdown's file name like so:
+```bash
+    ./mdp -file README.md
+```
+    the markdown file should open in your browser
+
+## Testing
+    Must have Go v1.19.3 
+```bash 
+    go test -v
+```
--- main.go
+++ main.go
@@ -4,26 +4,48 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
+	"os/exec"
+	"runtime"
 	"strconv"
 	"time"
+	"html/template"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 )
 
 const (
-	header = `<!DOCTYPE html>
-	<html>
-		<head>
-			<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-			<title>Markdown Preview Tool</title>
-		</head>
-		<body>`
-	footer = `</body
-				</html>`
+	defaultTemplate = `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+		<title>{{ .Title }}</title>
+		<style>
+			h1 {
+				color: blue
+			}
+			body {
+				font-family: 'Poppins', sans-serif;
+			}
+		</style>
+	</head>
+	<body>
+		{{ .Body }}
+	</body>
+	</html>`
 )
+
+type content struct {
+	Title string
+	Body template.HTML
+}
 
 func main() {
 	flag.Usage = func() {
@@ -34,6 +56,8 @@ func main() {
 	}
 
 	fileName := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("skip", false, "Skip auto-preview")
+	tFName := flag.String("t", "", "Alternate template name")
 	flag.Parse()
 
 	if *fileName == "" {
@@ -41,37 +65,107 @@ func main() {
 		os.Exit(1)
 	}
 
-	run(*fileName)
+	if err := run(*fileName, *tFName, os.Stdout, *skipPreview); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
-func run(fileName string) error {
+func run(fileName string, tFName string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(fileName)
 	if err != nil {
 		return err
 	}
 
-	htmlData := parseContent(input)
+	htmlData, err := parseContent(input, tFName)
+	if err != nil {
+		return err
+	}
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(fileName))
-	fmt.Println(outName)
+	tempFile, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
 
-	return saveHTML(outName, htmlData)
+	if err := tempFile.Close(); err != nil {
+		return err
+	}
+
+	outName := tempFile.Name()
+	fmt.Fprintln(out, outName)
+
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+	//defer os.Remove(outName)
+	return preview(outName)
 }
 
-func parseContent(input []byte) []byte {
+func parseContent(input []byte, tFname string) ([]byte, error) {
 	output := blackfriday.Run(input)
 	body := bluemonday.UGCPolicy().SanitizeBytes(output)
 
+	t, err := template.New("mdp").Parse(defaultTemplate)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if tFname != "" {
+		t, err = template.ParseFiles(tFname)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	c := content{
+		Title: "Markdown Preview Tool",
+		Body: template.HTML(body),
+	}
+
 	var buffer bytes.Buffer
 
-	buffer.WriteString(header)
-	buffer.Write(body)
-	buffer.WriteString(footer)
+	if err := t.Execute(&buffer, c); err != nil {
+		return nil, err
+	}
 
-	return buffer.Bytes()
+	return buffer.Bytes(), nil
 }
 
 func saveHTML(outName string, data []byte) error {
 	return os.WriteFile(outName, data, 0644)
 }
 
+func preview(fileName string) error {
+	cName := ""
+	cParams := []string {}
+
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default: 
+		return fmt.Errorf("OS not supported")
+	}
+
+	cParams = append(cParams, fileName)
+	cPath, err := exec.LookPath(cName)
+
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command(cPath, cParams...).Run()
+
+	// TODO: Refactor this when you learn how to handle signals
+	time.Sleep(2 * time.Second)
+	return err
+}
